8abe0832a53c6efa086f0f3862766c23d5be7c9c
Jacopo Moresco <morjaky@gmail.com>
2026-03-05 14:58:06 +0100

    Import and plot additional Sentinel bands
    Added imports for bands 4 and 8 from Sentinel data and updated plotting with corresponding color palettes.

--- Code/02_Script.R
+++ Code/02_Script.R
@@ -15,3 +15,25 @@ b2 = im.import("sentinel.dolomites.b2.tif")
 b3 = im.import("sentinel.dolomites.b3.tif")
 plot(b3, col=plasma(100))
 
+# importing band 4
+b4 = im.import("sentinel.dolomites.b4.tif")
+
+# importing band 8
+b8 = im.import("sentinel.dolomites.b8.tif")
+
+im.multiframe(2,2)
+
+# Esercizio multiframe, legende allineate con la lunghezza d'onda'
+clb <- colorRampPalette(c("darkblue", "blue", "lightblue"))(100)
+plot(b2, col=clb)
+
+clg <- colorRampPalette(c("darkgreen", "green", "lightgreen"))(100)
+plot(b3, col=clg)
+
+clr <- colorRampPalette(c("darkred", "red", "pink"))(100)
+plot(b4, col=clr)
+
+cln <- colorRampPalette(c("goldenrod3", "goldenrod2", "goldenrod"))(100)
+plot(b8, col=cln)
+
+
