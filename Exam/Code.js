23888b224ac5f3ce6b589b79e03cb986539f62be
Jacopo Moresco <morjaky@gmail.com>
2026-07-03 16:09:31 +0200

    Update Code.js

--- Exam/Code.js
+++ Exam/Code.js
@@ -15,9 +15,7 @@ Map.addLayer(geometry, {}, 'AOI Planinsko polje');
 // ==============================================
 // Caricamento Image Collection fase wet
 // ==============================================
-var s2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
-  .filterBounds(geometry)
-  .filterDate('2022-02-01', '2022-04-30') //periodo bagnato
-  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10)); 
 
-print('Numero immagini Sentinel-2:', s2.size());
+
+
+
