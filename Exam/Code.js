8f7a873848ba8c23b0c3cb74d75b25f7bf97d289
Jacopo Moresco <morjaky@gmail.com>
2026-07-06 18:05:49 +0200

    Update Code.js

--- Exam/Code.js
+++ Exam/Code.js
@@ -1,10 +1,11 @@
-
-
-
+// P - 
+// Telerilevamento Geo-ecologico in R
+// Jacopo Moresco
 
 // Le immagini sono state prese da: https://earthengine.google.com/
 // Di seguito il codice in JavaScript per scaricare le immagini di Sentinel-2
 
+// Area of Interest: 
 var aoi = ee.Geometry.Polygon(
         [[[14.075348963955573, 77.57823316518508],
           [14.075348963955573, 77.42532931273963],
@@ -16,13 +17,14 @@ Map.centerObject(aoi, 10);
 // ------------------------------------------------------
 // Estate 2016 
 // ------------------------------------------------------
-var s2_2016 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+var s2_2016 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') //Sentinel-2 Level-2A
   .filterBounds(aoi)
   .filterDate('2016-08-01', '2016-08-31')
-  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10));
+  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10)); // poca tolleranza 
 
 print('Conteggio immagini estate 2016:', s2_2016.size());
 
+// Crea il composito mediano e seleziona le bande necessarie
 var img_2016 = s2_2016.median()
   .select(['B2', 'B3', 'B4', 'B8', 'B11', 'B12'])
   .clip(aoi);
@@ -33,7 +35,7 @@ Map.addLayer(
   'RGB_2016_summer'
 );
 
-// Export to Google Drive
+// Esportazione su Google Drive
 Export.image.toDrive({
   image: img_2016,
   description: 'svalbard_glaciers_2016_late_summer',
@@ -55,6 +57,7 @@ var s2_2020 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
 
 print('Conteggio immagini estate 2020:', s2_2020.size());
 
+// Crea il composito mediano e seleziona le bande necessarie
 var img_2020 = s2_2020.median()
   .select(['B2', 'B3', 'B4', 'B8', 'B11', 'B12'])
   .clip(aoi);
@@ -65,7 +68,7 @@ Map.addLayer(
   'RGB_2020_summer'
 );
 
-// Export to Google Drive
+// Esportazione su Google Drive
 Export.image.toDrive({
   image: img_2020,
   description: 'svalbard_glaciers_2020_late_summer',
@@ -86,6 +89,7 @@ var s2_2024 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
 
 print('Conteggio immagini estate 2024:', s2_2024.size());
 
+// Crea il composito mediano e seleziona le bande necessarie
 var img_2024 = s2_2024.median()
   .select(['B2', 'B3', 'B4', 'B8', 'B11', 'B12'])
   .clip(aoi);
@@ -96,7 +100,7 @@ Map.addLayer(
   'RGB_2024_summer'
 );
 
-// Export to Google Drive
+// Esportazione su Google Drive
 Export.image.toDrive({
   image: img_2024,
   description: 'svalbard_glaciers_2024_late_summer',
