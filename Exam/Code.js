6f5956b801315c5872fe56ef01acb434247fc0ae
Jacopo Moresco <morjaky@gmail.com>
2026-07-06 09:59:43 +0200

    Update Code.js

--- Exam/Code.js
+++ Exam/Code.js
@@ -5,42 +5,104 @@
 // Le immagini sono state prese da: https://earthengine.google.com/
 // Di seguito il codice in JavaScript per scaricare le immagini di Sentinel-2
 
-// =====================
-// Vecchio codice 
-// =====================
-Map.centerObject(aoi, 12);
-var s2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
-  .filterBounds(aoi)
-  .filterDate(wet2023_start, wet2023_end)
-  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10))
-  .sort('CLOUDY_PIXEL_PERCENTAGE');
-print('Numero immagini Sentinel-2:', s2.size());
+var aoi = ee.Geometry.Polygon(
+        [[[14.075348963955573, 77.57823316518508],
+          [14.075348963955573, 77.42532931273963],
+          [14.573166957119636, 77.42532931273963],
+          [14.573166957119636, 77.57823316518508]]], null, false);
+          
+Map.centerObject(aoi, 10);
 
-// Median composite and band selection for RGB, NDVI and MNDWI in R
-var img = s2.median()
+// ------------------------------------------------------
+// Estate 2016 
+// ------------------------------------------------------
+var s2_2016 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+  .filterBounds(aoi)
+  .filterDate('2016-07-01', '2016-08-31')
+  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10));
+
+print('Conteggio immagini estate 2016:', s2_2016.size());
+
+var img_2016 = s2_2016.median()
   .select(['B2', 'B3', 'B4', 'B8', 'B11', 'B12'])
   .clip(aoi);
+  
+Map.addLayer(
+  img_2016,
+  {bands: ['B4', 'B3', 'B2'], min: 0, max: 8000},
+  'RGB_2016_summer'
+);
 
-//Visualizzazione a colori naturali
-Map.addLayer(img,{bands: ['B4', 'B3', 'B2'], min: 0, max: 2000},
-  'RGB_2023_wet');
-
-//Visualizzazione NIR, falsi colori
-Map.addLayer(img,{bands: ['B8', 'B4', 'B3'], min: 0, max: 2000},
-  'False_color_2023_wet');  
-
-// Export img to Google Drive
+// Export to Google Drive
 Export.image.toDrive({
-  image: img,
-  description: 'planinsko_2023_wet',
-  folder: 'telerilevamento_carsismo',
-  fileNamePrefix: 'planinsko_2023_wet',
+  image: img_2016,
+  description: 'svalbard_glaciers_2016_late_summer',
+  folder: 'telerilevamento_svalbard_glaciers',
+  fileNamePrefix: 'svalbard_glaciers_2016_late_summer',
   region: aoi,
   scale: 20,
   maxPixels: 1e13
 });
 
+// ------------------------------------------------------
+// Estate 2020 
+// ------------------------------------------------------
 
+var s2_2020 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+  .filterBounds(aoi)
+  .filterDate('2020-07-01', '2020-08-31')
+  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10));
 
+print('Conteggio immagini estate 2020:', s2_2020.size());
 
+var img_2020 = s2_2020.median()
+  .select(['B2', 'B3', 'B4', 'B8', 'B11', 'B12'])
+  .clip(aoi);
+  
+Map.addLayer(
+  img_2020,
+  {bands: ['B4', 'B3', 'B2'], min: 0, max: 8000},
+  'RGB_2020_summer'
+);
 
+// Export to Google Drive
+Export.image.toDrive({
+  image: img_2020,
+  description: 'svalbard_glaciers_2020_late_summer',
+  folder: 'telerilevamento_svalbard_glaciers',
+  fileNamePrefix: 'svalbard_glaciers_2020_late_summer',
+  region: aoi,
+  scale: 20,
+  maxPixels: 1e13
+});
+
+// ------------------------------------------------------
+// Estate 2024 
+// ------------------------------------------------------
+var s2_2024 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+  .filterBounds(aoi)
+  .filterDate('2024-07-01', '2024-08-31')
+  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10));
+
+print('Conteggio immagini estate 2024:', s2_2024.size());
+
+var img_2024 = s2_2024.median()
+  .select(['B2', 'B3', 'B4', 'B8', 'B11', 'B12'])
+  .clip(aoi);
+  
+Map.addLayer(
+  img_2024,
+  {bands: ['B4', 'B3', 'B2'], min: 0, max: 8000},
+  'RGB_2024_summer'
+);
+
+// Export to Google Drive
+Export.image.toDrive({
+  image: img_2024,
+  description: 'svalbard_glaciers_2024_late_summer',
+  folder: 'telerilevamento_svalbard_glaciers',
+  fileNamePrefix: 'svalbard_glaciers_2024_late_summer',
+  region: aoi,
+  scale: 20,
+  maxPixels: 1e13
+});
